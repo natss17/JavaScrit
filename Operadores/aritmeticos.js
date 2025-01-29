3e3774c44ca5e3cc1459ea3f3e001f25c9e0a9de
natss17 <nathh.mariana@outlook.com>
2025-01-29 10:54:09 -0300

    .

--- Operadores/aritmeticos.js
+++ Operadores/aritmeticos.js
@@ -23,4 +23,36 @@ console.log(sub)
 console.log(mult)
 console.log(div)
 console.log(resto)
+console.log("oi")
 
+
+
+// Atividade duas variáveis numéricas e exiba:)
+let variavel1= 5;
+let variavel2 = 8;
+
+
+console.log("soma:", variavel1 + variavel2);
+console.log("subtração:", variavel1 - variavel2);
+console.log("multiplicação:", variavel2 * variavel2);
+console.log("divisão:", variavel1 / variavel2);
+console.log("resto da divisão:", variavel1 % variavel2);
+
+// área de um retangulo 
+
+let largura = 6;
+let altura = 2;   
+
+let area = largura * altura;
+
+console.log("a área do retângulo é:", area);
+
+// Dobro e Metade
+let numero = 10;
+
+let dobro = numero * 2;
+let metade = numero / 2;
+
+console.log("Número:", numero);
+console.log("Dobro:", dobro);
+console.log("Metade:", metade);
---
+++ Operadores/relacionais.js
@@ -0,0 +1,31 @@
+/* > maior 
+>= maior ou igual 
+< menor
+<= menor ou igual 
+== igual
+! = diferente */ 
+
+let idade = 18
+let resultado = idade > 18 //maior
+let resultado2 = idade >= 18 // maior ou igual
+let resultado3 = idade < 18 // menor
+let resultado4 = idade <= 18 // menor lou igual
+let resultado5 = idade == 18 // igual
+let resultado6 = idade = ! 18 //diferente
+console.log (resultado)
+console.log(resultado2)
+console.log(resultado3)
+console.log(resultado4)
+console.log(resultado5)
+console.log(resultado6)
+
+console.log("-------------")
+console.log("outro exemplo")
+let dinheiro = 100
+let compra = 40
+let total = dinheiro - compra 
+let situação = total > 0 
+console.log(situação)
+console.log("outro exemplo")   // saber se um número é divisivel por outro
+let divisivel = 10 % 2 // vai dar 0 // 10 / 2 = 5 sobra nada
+console.log(divisivel)
