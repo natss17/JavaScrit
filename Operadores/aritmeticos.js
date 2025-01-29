625df004547f45892f4fd0e9a784c0b3d5a5c60c
natss17 <nathh.mariana@outlook.com>
2025-01-29 08:19:07 -0300

    Initial commit

---
+++ Operadores/aritmeticos.js
@@ -0,0 +1,26 @@
+console.log("Ola, Mundo!")
+
+let nome = "Juliana"
+var nomes = "Juliana"
+
+console.log(nome)
+
+let idade = 24
+
+console.log("Minha idade é", idade, "anos")
+
+let a = 3
+let b = 2
+
+let soma = a + b
+let sub = a - b
+let mult = a * b
+let div = a / b 
+let resto = a % b
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
+
