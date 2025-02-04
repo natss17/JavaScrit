6bb2107cf31e945486a0abed14222fb3135f5964
natss17 <nathh.mariana@outlook.com>
2025-02-04 10:06:45 -0300

    lista de Exercícios

---
+++ estrutura condicional/estrutura condicional.js
@@ -0,0 +1,35 @@
+// // // atividade  Estrutura Condicional, Comparar Idade para Entrada em Evento
+// // let idade = prompt ("Rock in rio 2025, informe sua idade :)")
+// // console.log("Sua idade é", idade)
+// // 
+// //     console.log("Entrada permitida, bom evento :)")
+// // } else {
+// //     console.log("Entrada proibida :(")
+// // }
+
+// // verifição de senha 
+// let senha = prompt ("digite sua senha :)")
+// console.log("Sua senha é", senha)
+// if (senha >= 1234){
+//     console.log("Senha correta ;)")
+// } else {
+//     console.log("Senha incorreta :(")
+// }
+
+//  verificar a temperatura 
+
+// let temperatura = prompt ("Ola, qual é a temperatura do dia de hj?")
+// if (temperatura >= 30 ) {
+// console.log ("Esta quente")
+// } else {
+//     console.log("Está frio")
+//     }
+
+//     Verificar se Número é Positivo ou Negativo
+
+let number = prompt ("Insira um numero")
+if (number >= 0) {
+console.log ("Número Positivo")
+} else {
+    console.log("Nuúmero Negativo")
+}
--- estrutura condicional/if.js
+++ estrutura condicional/if.js
@@ -2,7 +2,27 @@
 let idade = prompt ("Qual sua idade?")
 console.log("Sua idade é", idade)
 
-let numero = 7 
-if (numero > 10) {
-        console.log("é maior que 10")
+if (idade >= 18) {
+        console.log("Você é maior de idade")
+} else {
+        console.log("Você é menor de idade")
 }
+console.log("------------------------------------")
+// Verificando se um número é impar ou par
+let numero = prompt ("Escolha um número:")
+let resultado = numero % 2 
+if (resultado == 0) {
+        console.log("O número é par")
+} else {
+        console.log("O número é impar")
+}
+//  avaliação aluno 
+let nota1 = prompt ("Qual a primeira nota:")
+let nota2 = prompt ("Qual a segunda nota:")
+let total1 = (nota1 + nota2) / 2 
+if (total1 >= 7) {
+        console.log ("Aprovado")
+} else {
+        console.log ("Reprovado")
+}
+
