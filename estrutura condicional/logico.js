8a125ffac0ab3f23b8bd70b08e10c1b140cab94f
natss17 <nathh.mariana@outlook.com>
2025-02-05 11:13:51 -0300

    PPT

---
+++ estrutura condicional/logico.js
@@ -0,0 +1,70 @@
+// // compra de desconto 
+// let valor = 90 
+// let clientevip = false 
+
+// if (valor >= 100 || clientevip == true) {
+//     console.log("Você ganhou frete gratis")
+// } else {
+//     console.log("Tem que pagar o frete")
+// } 
+// // Número no intervalo 
+// let num = 10 
+// if (num >= 5 || num <= 20) {
+//     console.log("Funcionou")
+// } else {
+//     console.log("Não funcionou")
+// }
+
+
+// // 01 dirigir com carteira 
+// let idade = prompt ("Qual é a idade")
+// let carteira = true
+// if (idade >= 18 && carteira ==true ) {
+//     console.log("Você pode dirigir")
+// } else {
+//     console.log("Você não pode dirigir")   
+// }
+
+// // 02 Elegível para Promoção
+// let anos = prompt ("quantos anos de trabalho")
+// let projetos = prompt ("informe quantos projetos realizados")
+//     if (anos >= 5 || projetos > 10) {
+//         console.log("Você está elegível para promoção")
+// } else {
+//     console.log("Você não está elegível para promoção")
+// }
+
+// // 03 Entrar em um Evento
+// let id = prompt ("Qual sua idade?")
+// let id2 = true
+// if (id >= 18 && id2 > 30) {
+//     console.log("Você pode entrar no evento")
+// } else {
+//     console.log("Você não pode entrar no evento")
+// // }
+
+// // //  04 - Seleção de Funcionário
+
+// let age = prompt ("Coloque sua idade")
+// let age2 = prompt ("possui experiencia??")
+// if (age > 21 && age2 == true) {
+//     console.log("Você foi aceito para a vaga")
+// }else {
+//     console.log ("Você não foi aceito para a vaga")
+// }
+
+// // 05 -  Verificação de Login Avançada
+// let usuario = prompt ("Qual seu nome de usuario")
+// let senha = prompt ("Digite sua senha")
+// if (usuario == "adimin" && senha == 1234) {
+//     console.log ( "Login bem-sucedido")
+// } else {
+//     console.log ("Nome de usuário ou senha incorretos")
+// }
+
+// 06 -  Intervalo de Valores
+let number = prompt ("Digite um numero que esteja entre 10, 20, 30 e 50")
+let number2 = 20
+let number3 = 30 
+let number4 = 50
+if (number > && )
