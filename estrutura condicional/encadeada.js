6132539c87e3a1fdded7849f95064842c9a24e56
natss17 <nathh.mariana@outlook.com>
2025-02-05 08:32:06 -0300

    Estrutura Condicional Encadeada

---
+++ estrutura condicional/encadeada.js
@@ -0,0 +1,132 @@
+// // Número positivo, negativo ou zero 
+// let numero = prompt ("Escolha um número:") 
+// if (numero > 0) {
+//     console.log("Positivo")
+// } else if (numero < 0) {
+//     console.log("Negativo")
+// } else {
+//     console.log("Zero")
+// }
+// console.log("----------------------")
+// let nota = Number (prompt("Qual a nota???"))
+// if (nota >= 9) {
+//     console.log("Excelente")
+// } else if (nota >=7){
+//     console.log("Bom")
+// } else if (nota >= 5) {
+//     console.log("Regular")
+// } else {
+//     console.log("Ruim")
+// }
+// console.log("----------------------")
+// let valor = Number (prompt("Qual o valor da compra?"))
+// if (valor >= 300) {
+//     console.log("Ganhou 10% de desconto")
+// } else {
+//     console.log("Sem desconto")
+// }
+
+// classificação de idade
+
+// let idade = prompt ("digite sua idade:") 
+// if (idade <= 12) {
+//     console.log("Criança")
+// } else if (idade < 17) {
+//     console.log("Adolescente")
+// } else if (idade <= 59)  {
+//     console.log("Adulto")
+// } else if (idade >= 60) {
+//     console.log("Idoso")
+// }
+
+// // avaliação de temperatura 
+// let grau = prompt ("Digite a temperatura")
+// if (grau<= 10) {
+//     console.log("Muito frio")
+// } else if (grau <= 20) {
+//     console.log("Frio") 
+// } else if (grau < 30) {
+//     console.log("Agradável")
+// } else if (grau >= 30) {
+//     console.log ("Muito quente")
+// // }
+
+// // // Classificação de Turno
+// let turno = prompt (" Insira string representando o turno de estudo:") 
+// if (turno = "M") {
+//     console.log("Bom dia matutino :)")
+// } else if (turno = "V") {
+//     console.log("Boa tarde vespertino")
+// } else if (turno = "N")  {
+//     console.log("Boa noite noturno")
+// } else if (turno = valor) {
+//     console.log("Turno inválido")
+// }
+
+// //  // Desconto Progressivo
+// let valortotal = prompt ("Insira o valor da compra:") 
+// if (valortotal <= 100 ) {
+//     console.log("wow vc ganhou 5% de desconto ;)")
+// } else if (valortotal <= 200) {
+//     console.log("Parabens vc ganhou 10 % de desconto")
+// } else if (valortotal > 200) {
+//     console.log("Eba voce ganhou 15% de desconto")
+// }
+
+// //  Sistema de Notas Escolares
+// let nota = prompt ("Digite sua nota")
+// if (nota > 90) {
+//     console.log ("A");
+// } else if (nota > 80) {
+//     console.log ("B");
+// } else if (nota > 70) {
+//     console.log ("C");
+// } else if (nota > 60) {
+//     console.log ("D");
+// } else {
+//     console.log ("F");
+// }
+
+// // verificar dia da semana 
+// let numero = prompt ("Escolha um dia dA semana:") 
+//     if (numero === 1) {
+//         console.log ("Domingo")
+//     } else if (numero === 2) {
+//         console.log ("Segunda-feira")
+//     } else if (numero === 3) {
+//         console.log ("Terça-feira")
+//     } else if (numero === 4) {
+//         console.log ("Quarta-feira")
+//     } else if (numero === 5) {
+//         console.log ("Quinta-feira")
+//     } else if (numero === 6) {
+//         console.log ("Sexta-feira")
+//     } else if (numero === 7) {
+//         console.log ("Sábado")
+// //     } else {
+// //         console.log ("Número inválido! Digite um número de 1 a 7.")
+// //     }
+
+// // Classificação de Pistas em Corridas
+// let distancia = prompt ("digite a distancia")
+// if (distancia < 400){
+//     console.log("Pista curta") 
+// } else if (distancia < 800) {
+//     console.log("Pista média")
+// } else if (distancia < 1500){
+//     console.log("Pista longa")
+// }else if (distancia > 1500) {
+//     console.log("Pista muito longa")
+// }
+
+// Pontuação de Jogo de Vídeo Game
+let Pontuação = prompt ("digite sua pontuação")
+if (Pontuação < 1000){
+    console.log("Iniciante")
+} else if (Pontuação < 5000){
+    console.log("Intermediário")
+} else if (Pontuação < 10000) {
+    console.log("Avançado")
+} else if (Pontuação > 10000) {
+    console.log("Mestre")
+}
