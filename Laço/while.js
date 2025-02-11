30e2ca56fba1d00911484086bf1a58547e26756e
natss17 <nathh.mariana@outlook.com>
2025-02-11 11:47:19 -0300

    while

--- Laço/while.js
+++ Laço/while.js
@@ -1,31 +1,94 @@
-let contador = 0 // inicia a contagem do zero
-while(contador <= 5) {
-    // continua enquanto a condição dor verdade 
-        console.log(contador)
-        // console.log (conatdor)  mostra o num atual
-          contador++ // atualiza o valor do contador 
-}
-////////////////////////////////////////////
-let controle = 5
-while (controle >= 0) {
-    console.log(controle)
-    controle --
-// podemos fazer ele contar de forma regressiva
-}
-console.log("terceiro exemplo")
-let numero = 3 
-let multiplicador = 1
-while (multipliador <= 4) {
-    let resultado = numero * multiplicador
-    // console.log (resultado) 
-   console.log (numero,  "x", multiplicador, "=", resultado) // assim fica mais bonito 
-    multiplicador += 1 // outra opção para o ++
-}
-////////////////////////
-let palavra = prompt ("Digite a palavra")
-while (palavra != "oi") {
-    palavra = prompt ("Tente novamente:")
-    if (palavra == "oi") {
-        console.log("Acerto")
-    }
+// let contador = 0 // inicia a contagem do zero
+// while(contador <= 5) {
+//     // continua enquanto a condição dor verdade 
+//         console.log(contador)
+//         // console.log (conatdor)  mostra o num atual
+//           contador++ // atualiza o valor do contador 
+// }
+// ////////////////////////////////////////////
+// let controle = 5
+// while (controle >= 0) {
+//     console.log(controle)
+//     controle --
+// // podemos fazer ele contar de forma regressiva
+// }
+// console.log("terceiro exemplo")
+// let numero = 3 
+// let multiplicador = 1
+// while (multipliador <= 4) {
+//     let resultado = numero * multiplicador
+//     // console.log (resultado) 
+//    console.log (numero,  "x", multiplicador, "=", resultado) // assim fica mais bonito 
+//     multiplicador += 1 // outra opção para o ++
+// }
+// ////////////////////////
+// let palavra = prompt ("Digite a palavra")
+// while (palavra != "oi") {
+//     palavra = prompt ("Tente novamente:")
+//     if (palavra == "oi") {
+//         console.log("Acerto")
+//     }
+// // }
+
+// // 1
+// let contador = 1;
+// while (contador <= 10) {
+//     console.log(contador);
+//     contador++
+    
+//
+
+// 02
+//  let num = prompt ("Digite um numero")
+//  let multipliador1 = 1
+//  while (multipliador1 <=10){
+//     let resultado = num * multipliador1
+//     console.log(num, "x", "=", resultado)
+//     multi ++
+//  }
+
+// // 03
+// let contagem = 10
+// while (contagem >= 1) {
+//     console.log(contagem)
+//     contagem --
+// }
+
+// // 04
+// let senha = prompt ("Digite sua senha :)")
+// while (senha != 12345)  {
+//     senha = prompt ("tente novamente")
+//     if (senha == 12345){
+//     console.log("Acesso liberado")
+// }
+// }
+
+// //05
+// let contador = 50
+// while (contador <= 100){
+//     console.log (contador)
+//     contador ++
+// }
+
+// //06 
+// let num = 0
+// while (num <= 100) {
+//   console.log(num)
+//   num += 5
+// }
+// console.log("-------------------------------------------------")
+
+// // 07
+// let mensagem =0
+// while (mensagem < 5){
+//     console.log ("Eu gosto de JavaScript")
+//     mensagem ++
+// }
+
+// 08
+let name = prompt ("Escreva seu nome")
+let numero = prompt ("Escreva um numero")
+while (numero >= 1){
+    console.log (name)
+    numero --
 }
