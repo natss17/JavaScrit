d5c1e33c46f22e0f2b1893a33ebb0caa60f4aab2
natss17 <nathh.mariana@outlook.com>
2025-02-25 08:46:40 -0300

    ppt

--- PPT/ppt.js
+++ PPT/ppt.js
@@ -1,24 +1,46 @@
 // Jogo pedra, papel, tesoura
-const escolhajogador = prompt ("Escolha pedra, papel ou tesoura:")
+// Variáveis para armazenar os pontos do jogafdor e pc
+let placarJogador = 0
+let placarPC = 0
+
+//Variável para armazenar as rodadas 
+let rodadasd = Number(prompt("Quantas vezes deseja jogar?"))
+
+//Laço de repetição que fará as rodadas
+for (let i = 0; i < rodadasd; i++) {
+
+
+const escolhajogador = prompt("Escolha pedra, papel ou tesoura:")
 // Solicita que o usuário informe qual ele quer 
 
 const escolhaPC = ["pedra", "papel", "tesoura"]
 [Math.floor(Math.random() * 3)]
 // Gera uma escolha aleatoria para o computador
 // Criamos um array com as opções e usamos o random para escolher uma de forma aleatoria
-alert ( `Você escolheu:  ${escolhajogador}`)
-alert ( `O COMPUTADOR escolheu:  ${escolhaPC}`)
+alert(`Você escolheu:  ${escolhajogador}`)
+alert(`O COMPUTADOR escolheu:  ${escolhaPC}`)
 // Mostrando qual foi a nossa escolha e qual foi a escolha o computador
 
 // Compara se a nossa escolha foi a do computador
-if (escolhajogador == escolhaPC){
+if (escolhajogador == escolhaPC) {
     alert("Empate")
-}else if (escolhajogador == "pedra" && escolhaPC == "tesoura") { //criamos a primeira verificação de verificação
+} else if (escolhajogador == "pedra" && escolhaPC == "tesoura") { //criamos a primeira verificação de verificação
     alert("Você venceu")
+    placarJogador++
 } else if (escolhajogador == "tesoura" && escolhaPC == "papel") { //criamos a segunda verificação de verificação
     alert("Você venceu")
+    placarJogador++
 } else if (escolhajogador == "papel" && escolhaPC == "pedra") { //criamos a terceira verificação de verificação
     alert("Você venceu")
+    placarJogador++
 } else {
-    alert("Você perdeu!!!!!!!!!!!!!!!!!!!!!!LOSER")
+    alert("Você perdeu")
+    placarPC++
 }
+// Exibe o placar no console 
+console.log("Placar jogador:", placarJogador)
+console.log("Placar computador:" ,placarPC)
+}
+
+//placar final
+alert(`Fim de jogo! Placar final: Jogador ${placarJogador} x ${placarPC} Computador`)
---
+++ funções/anonima.js
@@ -0,0 +1,21 @@
+// função anonima 
+let saudacao = function(nome){
+    console.log ("Oi ")
+}
+
+//Arrow Function 
+let adicao = (a,b) => {
+    let resultado = a + b
+    return resultado
+}
+console.log (adicao(6,4))
+
+// função anonima 
+let dobro = function(numero) {
+    let resultado = numero * 2
+    return resultado 
+}
+console.log (vezesDois(8))
+
+//lista de exercicio 
+ 
