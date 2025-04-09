8181658c8d4b03d234af923fdb52be9007c3cac4
natss17 <nathh.mariana@outlook.com>
2025-04-09 08:10:30 -0300

    .

---
+++ drag dropp/script.js
@@ -0,0 +1,21 @@
+// DRAG DROP -> PROJETO -> SCRIPT.JS
+const tarefas = document.querySelectorAll(".tarefa")
+const colunas = document.querySelectorAll(".tarefas")
+
+tarefas.forEach(tarefa => {
+  tarefa.addEventListener("dragstart", () => {
+    tarefa.classList.add("arrastando");
+  });
+
+  tarefa.addEventListener("dragend", () => {
+    tarefa.classList.remove("arrastando")
+  })
+})
+colunas.forEach(coluna => {
+    coluna.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      const tarefaArrastando = document.querySelector(".arrastando")
+      coluna.appendChild(tarefaArrastando)
+    })
+  })
+  
